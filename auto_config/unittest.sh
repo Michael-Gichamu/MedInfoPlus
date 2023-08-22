295b3fcb88df7975ae0a851246434017ed740e02
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-22 05:04:54 +0000

    Run unittests

--- auto_config/unittest.sh
+++ auto_config/unittest.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 # Execute the test configuration script
-source test_config.sh
+source auto_config/test_config.sh
 printenv | grep MedInfoPlus_ENV
 
 # Run tests
+echo "BaseModel Class Tests"
 python3 -m unittest tests.test_models.test_base_model
+
+echo "MedicalArticle Class Tests"
 python3 -m unittest tests.test_models.test_medical_article
+
+echo "Resource Class Tests"
+python3 -m unittest tests.test_models.test_resource
+
 # Back to development
-source dev_config.sh
+source auto_config/dev_config.sh
 printenv | grep MedInfoPlus_ENV
