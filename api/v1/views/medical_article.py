8617ab79a96c3d1c276f33bed9f86f8078108463
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-24 06:05:07 +0000

    medical article api route

--- api/v1/views/medical_article.py
+++ api/v1/views/medical_article.py
@@ -2,7 +2,7 @@
 """
 Flask route that returns json medicalarticle response
 """
-from flask import request
+from flask import abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
 from models.medical_article import MedicalArticle
@@ -21,21 +21,41 @@ def get_medicalarticles():
     return jsonify(medicalarticles_list)
 
 
-@app_views.route('/medicalarticles/<medicalarticle_name>', methods=['GET'])
-def get_medicalarticle(medicalarticle_name):
+@app_views.route('/medicalarticles/<medicalarticle_id>', methods=['GET'])
+def get_medicalarticle(medicalarticle_id):
     """
     Retrieves a medical article.
     """
-    medicalarticle_name = medicalarticle_name.replace(' ', '')
-    medicalarticle = storage.get(MedicalArticle, name=medicalarticle_name)
+    medicalarticle = storage.get(MedicalArticle, medicalarticle_id)
     if medicalarticle is None:
         abort(404, 'Not found')
-    return jsonify(medical_article.to_dict())
+    return jsonify(medicalarticle.to_dict())
 
 
-@app_views.route('/medicalarticles/<article_name>/categories/medicalarticles', methods=['GET'])
-    medicalarticle_name = medicalarticle_name.replace(' ', '')
-    medicalarticle = storage.get(MedicalArticle, name=medicalarticle_name)
+@app_views.route('/medicalarticles/<medicalarticle_id>/categories/medicalarticles', methods=['GET'])
+def get_medicalarticles_of_category(medicalarticle_id):
+    """
+    Get medicalarticles of the same category.
+    """
+    medicalarticle = storage.get(MedicalArticle, medicalarticle_id)
     if medicalarticle is None:
         abort(404, 'Not found')
-@app_views.route('/medicalarticles/<article_name>/resource/medicalarticles', methods=['GET'))
+    medical_articles = storage.all(MedicalArticle)
+    category_medical_articles = [medical_article.to_dict()
+                                 for medical_article in medical_articles.values()
+                                 if medical_article.category == medicalarticle.category]
+    return jsonify(category_medical_articles)
+
+@app_views.route('/medicalarticles/<medicalarticle_id>/resource/medicalarticles', methods=['GET'])
+def get_medicalarticles_of_resource(medicalarticle_id):
+    """
+    Get medicalarticles of the same resource.
+    """
+    medicalarticle = storage.get(MedicalArticle, medicalarticle_id)
+    if medicalarticle is None:
+        abort(404, 'Not found')
+    medical_articles = storage.all(MedicalArticle)
+    resource_medical_articles = [medical_article.to_dict()
+                                 for medical_article in medical_articles.values()
+                                 if medical_article.resource_Id == medicalarticle.resource_Id]
+    return jsonify(resource_medical_articles)
