a54bea50cb38ba8f63aab8b096080feb1ec3c90c
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-24 06:06:45 +0000

    MedicalArticles Model Class

--- models/medical_article.py
+++ models/medical_article.py
@@ -2,14 +2,15 @@
 """Class MedicalArticle"""
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Text
+from sqlalchemy import Column, String, ForeignKey, Integer
 
 
 class MedicalArticle(BaseModel, Base):
     """Representation of medicalarticle"""
     __tablename__ = 'medicalarticle'
     title = Column(String(60), nullable=False)
-    category = Column(String(60), nullable=True)
+    category = Column(String(60), nullable=False)
+    query_count = Column(Integer, nullable=False, default=0)
     resource_Id = Column(String(60), ForeignKey('resource.id'), nullable=False)
 
     def __init__(self, *args, **kwargs):
