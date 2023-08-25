630e380356f19d18471cc569857df6638ab92a87
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-25 12:41:15 +0300

    Update setup_mysql_test.sql

--- Config_Management/setup_mysql_test.sql
+++ Config_Management/setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- prepares a MySQL server for this project
 
 CREATE DATABASE IF NOT EXISTS MedInfoPlus_test_db;
-CREATE USER IF NOT EXISTS 'MedInfoPlus_test'@'%' IDENTIFIED BY 'MedInfoPlus_test_pwd';
+CREATE USER IF NOT EXISTS 'MedInfoPlus_test'@'localhost' IDENTIFIED BY 'MedInfoPlus_test_pwd';
 GRANT ALL PRIVILEGES ON MedInfoPlus_dev_db.* TO 'MedInfoPlus_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'MedInfoPlus_test'@'localhost';
 FLUSH PRIVILEGES;
