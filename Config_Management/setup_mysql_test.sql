4008bfe2823efb27e8280d27251f2f805d8a02a0
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-26 13:44:58 +0300

    Sets Up test Database

--- Config_Management/setup_mysql_test.sql
+++ Config_Management/setup_mysql_test.sql
@@ -2,6 +2,6 @@
 
 CREATE DATABASE IF NOT EXISTS MedInfoPlus_test_db;
 CREATE USER IF NOT EXISTS 'MedInfoPlus_test'@'localhost' IDENTIFIED BY 'MedInfoPlus_test_pwd';
-GRANT ALL PRIVILEGES ON MedInfoPlus_dev_db.* TO 'MedInfoPlus_test'@'localhost';
+GRANT ALL PRIVILEGES ON MedInfoPlus_test_db.* TO 'MedInfoPlus_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'MedInfoPlus_test'@'localhost';
 FLUSH PRIVILEGES;
