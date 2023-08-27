fa9e7807c6c89f6112175daae2c97a7cab7d1849
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-27 20:01:58 +0300

    MedicalArticle Model Class

--- models/medical_article.py
+++ models/medical_article.py
@@ -11,7 +11,9 @@ class MedicalArticle(BaseModel, Base):
     title = Column(String(60), nullable=False)
     category = Column(String(60), nullable=False)
     query_count = Column(Integer, nullable=False, default=0)
-    resource_Id = Column(String(60), ForeignKey('resource.id'), nullable=False)
+    summary = Column(String(400), nullable=True)
+    image = Column(String(60), nullable=True)
+    resource_Id = Column(String(60), ForeignKey('resource.id', ondelete="CASCADE"), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
