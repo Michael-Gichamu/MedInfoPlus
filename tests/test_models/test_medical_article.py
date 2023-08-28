e04d0fe8e48069d6be73bf5bb28789511f0a9dee
Michael-Gichamu <michaelmatere606@gmail.com>
2023-08-28 07:37:28 +0300

    Unittests for MedicalArticle Class

--- tests/test_models/test_medical_article.py
+++ tests/test_models/test_medical_article.py
@@ -39,6 +39,16 @@ class TestMedicalArticle(unittest.TestCase):
         self.assertTrue(hasattr(self.medicalarticle, "query_count"))
         self.assertEqual(self.medicalarticle.query_count, None)
 
+    def test_summary(self):
+        """Test that MedicalArticle has attribute summary and its none"""
+        self.assertTrue(hasattr(self.medicalarticle, "summary"))
+        self.assertEqual(self.medicalarticle.summary, None)
+
+    def test_image(self):
+        """Test that MedicalArticle has attribute image and its none"""
+        self.assertTrue(hasattr(self.medicalarticle, "image"))
+        self.assertEqual(self.medicalarticle.image, None)
+
     def test_resource_Id_attr(self):
         """Test that MedicalArticle has attribute resource_Id and its none"""
         self.assertTrue(hasattr(self.medicalarticle, "resource_Id"))
@@ -61,6 +71,8 @@ class TestMedicalArticle(unittest.TestCase):
         article_data = {
             'title': 'SampleArticle',
             'category': 'SampleCategory',
+            'summary': 'Sample has a summary',
+            'image': 'samplearticle.jpg',
             'resource_Id': '{}'.format(resource.id)
         }
         article = MedicalArticle(**article_data)
@@ -72,6 +84,8 @@ class TestMedicalArticle(unittest.TestCase):
         self.assertIsNotNone(fetched_article)
         self.assertEqual(fetched_article.category, 'SampleCategory')
         self.assertEqual(fetched_article.query_count, 0)
+        self.assertEqual(fetched_article.summary, 'Sample has a summary')
+        self.assertEqual(fetched_article.image, 'samplearticle.jpg')
         self.assertEqual(fetched_article.resource_Id, resource.id)
 
         self.tester.teardown()
