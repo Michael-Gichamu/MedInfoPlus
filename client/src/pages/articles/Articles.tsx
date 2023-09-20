f1344bf26687759c57ebecdf27d5158165940cdf
AMO15310 <wachiraamos402@gmail.com>
2023-09-20 14:10:02 +0300

    fix:article-page

--- client/src/pages/articles/Articles.tsx
+++ client/src/pages/articles/Articles.tsx
@@ -29,8 +29,7 @@ export const ArticlesComponent: React.FC = (): JSX.Element => {
   const getAlldata = async () => {
     try {
       const resp = await datafromServer(`medicalarticles/${id}`);
-      console.log(resp.content);
-      setTitle(resp.category);
+      setTitle(resp.title);
       setContent(resp.content);
     } catch (error) {
       // setError(error);
@@ -45,7 +44,7 @@ export const ArticlesComponent: React.FC = (): JSX.Element => {
     getData();
     getAlldata();
   }, []);
-  const text = "Diabetes";
+  const text = "";
   if (!error) {
     return (
       <>
@@ -64,7 +63,7 @@ export const ArticlesComponent: React.FC = (): JSX.Element => {
           </div>
           <div className="things2know flex  justify-between ml-5 ">
             <div className="text flex flex-col w-[70%]">
-              <p className="text font-medium text-lg  mt-[4rem]">
+              <p className="text font-medium text-lg  mt-[4rem] mb-2">
                 Things to Know
               </p>
               <div className=" flex-wrap flex ">
@@ -72,30 +71,9 @@ export const ArticlesComponent: React.FC = (): JSX.Element => {
                   className=""
                   dangerouslySetInnerHTML={{ __html: content }}
                 ></div>
-                {/* <section>
-                  <h2>Fact : Chronic Condition</h2>
-                  <p>
-                    Diabetes is a chronic health condition affecting blood sugar
-                    levels.
-                  </p>
-                </section> */}
               </div>
             </div>
-            {/* {content.map((section: any, index: any) => (
-              <div key={index} className="text flex flex-col w-[70%]">
-                <p className="text font-medium text-lg mt-[4rem]">
-                  {section.title}
-                </p>
-                <div className="flex-wrap flex">
-                  {section.content.map((subsection: any, subIndex: any) => (
-                    <section key={subIndex}>
-                      <h2>{subsection.title}</h2>
-                      <p>{subsection.content}</p>
-                    </section>
-                  ))}
-                </div>
-              </div>
-            ))} */}
+
             <div className="image w-[30%]  mt-[5rem] ml-10">
               <div className=" mb-4 underline">Recomended Articles </div>
               <div className="">{FirstTitle}</div>
@@ -110,86 +88,6 @@ export const ArticlesComponent: React.FC = (): JSX.Element => {
               </div>
             </div>
           </div>
-          {/* <div className="whatislungcancer flex  justify-between ml-5 pb-4 ">
-            <div className="text flex flex-col w-[70%]">
-              <p className="text font-medium text-lg  mt-[4rem]">
-                What is {text}
-              </p>
-              <div className=" flex-wrap flex ">
-                Diabetes is a chronic medical condition that affects how your
-                body processes glucose (sugar), a vital source of energy.
-                <section>
-                  <h2>Symptoms and Diagnosis</h2>
-                  <p>
-                    Common symptoms of diabetes include increased thirst,
-                    frequent urination, and unexplained weight loss.
-                  </p>
-                  <p>
-                    Diagnosis involves blood tests to measure blood sugar
-                    levels.
-                  </p>
-                </section>
-                <section>
-                  <h2>Treatment</h2>
-                  <p>
-                    Treatment for diabetes may include lifestyle changes,
-                    medication, and insulin therapy.
-                  </p>
-                </section>
-              </div>
-            </div>
-           
-          </div> */}
-          {/* <div className="whatislungcancer flex  justify-between ml-5 pb-4 ">
-            <div className="text flex flex-col w-[70%]">
-              <p className="text font-medium text-lg  mt-[4rem]">
-                What are the causes of {text}
-              </p>
-              <div className=" flex-wrap flex ">
-                <h1>Causes of Diabetes</h1>
-
-                <h2>Type 1 Diabetes:</h2>
-                <p>
-                  <strong>Autoimmune Reaction:</strong> Type 1 diabetes occurs
-                  when the immune system attacks and destroys insulin-producing
-                  cells in the pancreas.
-                </p>
-
-                <h2>Type 2 Diabetes:</h2>
-                <p>
-                  <strong>Insulin Resistance:</strong> Type 2 diabetes is linked
-                  to insulin resistance, where cells don't respond effectively
-                  to insulin.
-                </p>
-                <p>
-                  <strong>Obesity:</strong> Excess body weight, especially
-                  abdominal obesity, increases the risk of type 2 diabetes.
-                </p>
-                <p>
-                  <strong>Physical Inactivity:</strong> Lack of regular physical
-                  activity contributes to insulin resistance.
-                </p>
-                <p>
-                  <strong>Unhealthy Diet:</strong> A diet high in sugars and
-                  unhealthy fats raises the risk of type 2 diabetes.
-                </p>
-                <p>
-                  <strong>Genetics:</strong> Family history and genetics play a
-                  role in determining the risk of type 2 diabetes.
-                </p>
-
-                <h2>Gestational Diabetes:</h2>
-                <p>
-                  <strong>Hormonal Changes:</strong> During pregnancy, hormonal
-                  changes can lead to insulin resistance and gestational
-                  diabetes.
-                </p>
-              </div>
-            </div>
-            <div className="image w-[30%]  mt-[5rem] ml-10 ">
-              
-            </div>
-          </div> */}
         </div>
       </>
     );
